95f50e6f10be7d0425676cc4ec07ac97ff9c3179
Liu Feng <129512581+JuderLiu@users.noreply.github.com>
2023-04-03 20:10:33 +0800

    Update GRS_main.m

--- scripts/GARS-Trade-off/GRS_main.m
+++ scripts/GARS-Trade-off/GRS_main.m
@@ -12,7 +12,7 @@ tic
 % Get acess to the current path and add the files in the 'main' folder to path
 current_path = pwd; 
 [parent_path, ~, ~] = fileparts(current_path); 
-addpath(strcat(parentpath, "\main"));
+addpath(strcat(parent_path, "\main"));
 
 %% Output
 % Normalized average total cost and average target deviation of the G-ARS
